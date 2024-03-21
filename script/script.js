833578f85e0fb1240bc3bc8c1893d6bfe8374cd7
lubara <ezequieladriani1901@gmail.com>
2024-03-21 18:34:39 +0100

    segundaPre-entrega

--- script/script.js
+++ script/script.js
@@ -18,10 +18,41 @@ function posibleCursada(alumnos) {
 let alumnosInscriptos = [];
   
 for (let i=1; i <= 10; i+=1){
-    var alumno = prompt("nombre");
+    var alumno = prompt("Ingrese le nombre del alumno");
     alumnosInscriptos.push(alumno);
 }
 
 
 posibleCursada(alumnosInscriptos);
 document.write("Los alumnos que comenzaran el presente curso son:"+ "<br>" + alumnosInscriptos.join("<br>"));
+
+
+document.write("<br>"+"--------------------------------------0---------------------------------"+"<br>")
+
+document.write("<br>" + "Felicitaciones, ha terminado la cursada. El promedio de nota de sus alumnos es el siguiente:");
+
+class Alumno {
+  constructor(nombre) {
+      this.nombre = nombre;
+      this.parcial1 = parseFloat(prompt(`Ingrese la nota del parcial 1 para ${this.nombre}:`));
+      this.parcial2 = parseFloat(prompt(`Ingrese la nota del parcial 2 para ${this.nombre}:`));
+  }
+
+  calcularPromedio() {
+      const promedio = (this.parcial1 + this.parcial2) / 2;
+      return promedio.toFixed(2);
+  }
+}
+
+const alumnos = [];
+for (let i = 0; i < alumnosInscriptos.length; i++) {
+  const nombreAlumno = alumnosInscriptos[i];
+  const alumno = new Alumno(nombreAlumno);
+  alumnos.push(alumno);
+}
+
+document.write("<br>"+"Promedios de los alumnos:<br>");
+for (const alumno of alumnos) {
+  const promedio = alumno.calcularPromedio();
+  document.write(`${alumno.nombre}: ${promedio}<br>`);
+}
