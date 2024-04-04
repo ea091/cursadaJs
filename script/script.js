43266b8dd4aa6b89886158ed9557ef3b35e27b10
lubara <ezequieladriani1901@gmail.com>
2024-04-04 18:02:57 +0200

    tercera entrega

--- script/script.js
+++ script/script.js
@@ -1,58 +1,114 @@
-const cantidadNecesaria = 10;
+// const cantidadNecesaria = 10;
 
+
+// function contarAlumnos(alumnos) {
+//   return alumnos.length;
+// }
+
+
+// function posibleCursada(alumnos) {
+//   const cantidadAlumnos = contarAlumnos(alumnos);
+//   if (cantidadAlumnos >= cantidadNecesaria) {
+//     document.write("Felicitaciones tu cursada tiene las inscripciones suficientes!"+ "<br>");
+//   } else {
+//     document.write("La cursada no puede iniciar, faltan alumnos.");
+//   }
+// }
+
+// let alumnosInscriptos = [];
+  
+// for (let i=1; i <= 10; i+=1){
+//     var alumno = prompt("Ingrese le nombre del alumno");
+//     alumnosInscriptos.push(alumno);
+// }
+
+
+// posibleCursada(alumnosInscriptos);
+// document.write("Los alumnos que comenzaran el presente curso son:"+ "<br>" + alumnosInscriptos.join("<br>"));
+
+
+// document.write("<br>"+"--------------------------------------0---------------------------------"+"<br>")
+
+// document.write("<br>" + "Felicitaciones, ha terminado la cursada. El promedio de nota de sus alumnos es el siguiente:");
+
+// class Alumno {
+//   constructor(nombre) {
+//       this.nombre = nombre;
+//       this.parcial1 = parseFloat(prompt(`Ingrese la nota del parcial 1 para ${this.nombre}:`));
+//       this.parcial2 = parseFloat(prompt(`Ingrese la nota del parcial 2 para ${this.nombre}:`));
+//   }
+
+//   calcularPromedio() {
+//       const promedio = (this.parcial1 + this.parcial2) / 2;
+//       return promedio.toFixed(2);
+//   }
+// }
+
+// const alumnos = [];
+// for (let i = 0; i < alumnosInscriptos.length; i++) {
+//   const nombreAlumno = alumnosInscriptos[i];
+//   const alumno = new Alumno(nombreAlumno);
+//   alumnos.push(alumno);
+// }
+
+// document.write("<br>"+"Promedios de los alumnos:<br>");
+// for (const alumno of alumnos) {
+//   const promedio = alumno.calcularPromedio();
+//   document.write(`${alumno.nombre}: ${promedio}<br>`);
+// }
+
+
+const cantidadNecesaria = 10;
 
 function contarAlumnos(alumnos) {
   return alumnos.length;
 }
 
-
 function posibleCursada(alumnos) {
   const cantidadAlumnos = contarAlumnos(alumnos);
   if (cantidadAlumnos >= cantidadNecesaria) {
-    document.write("Felicitaciones tu cursada tiene las inscripciones suficientes!"+ "<br>");
+    document.write("Felicitaciones tu cursada tuvo las inscripciones suficientes!. Hciendo click en el nombre de los alumnos veras sus notas y promedios." + "<br>");
   } else {
     document.write("La cursada no puede iniciar, faltan alumnos.");
   }
 }
 
-let alumnosInscriptos = [];
-  
-for (let i=1; i <= 10; i+=1){
-    var alumno = prompt("Ingrese le nombre del alumno");
-    alumnosInscriptos.push(alumno);
-}
+let alumnosInscriptos = JSON.parse(localStorage.getItem('alumnosInscriptos')) || [];
 
+if (alumnosInscriptos.length === 0) {
+  for (let i = 1; i <= 10; i += 1) {
+    var nombreAlumno = prompt("Ingrese el nombre del alumno");
+    var parcial1 = parseFloat(prompt(`Ingrese la nota del parcial 1 para ${nombreAlumno}:`));
+    var parcial2 = parseFloat(prompt(`Ingrese la nota del parcial 2 para ${nombreAlumno}:`));
+    alumnosInscriptos.push({ nombre: nombreAlumno, parcial1: parcial1, parcial2: parcial2 });
+  }
+}
 
 posibleCursada(alumnosInscriptos);
-document.write("Los alumnos que comenzaran el presente curso son:"+ "<br>" + alumnosInscriptos.join("<br>"));
 
+const listaAlumnos = document.getElementById('alumnos-lista');
 
-document.write("<br>"+"--------------------------------------0---------------------------------"+"<br>")
+alumnosInscriptos.forEach((alumno, index) => {
+  const listItem = document.createElement('li');
+  listItem.textContent = alumno.nombre;
+  listItem.addEventListener('click', () => {
+    abrirVentanaNotas(alumnosInscriptos[index]);
+  });
+  listaAlumnos.appendChild(listItem);
+});
 
-document.write("<br>" + "Felicitaciones, ha terminado la cursada. El promedio de nota de sus alumnos es el siguiente:");
-
-class Alumno {
-  constructor(nombre) {
-      this.nombre = nombre;
-      this.parcial1 = parseFloat(prompt(`Ingrese la nota del parcial 1 para ${this.nombre}:`));
-      this.parcial2 = parseFloat(prompt(`Ingrese la nota del parcial 2 para ${this.nombre}:`));
-  }
-
-  calcularPromedio() {
-      const promedio = (this.parcial1 + this.parcial2) / 2;
-      return promedio.toFixed(2);
-  }
+function abrirVentanaNotas(alumno) {
+  const ventanaNotas = window.open('', 'Notas', 'width=400,height=400');
+  ventanaNotas.document.write(`<h1>Notas de ${alumno.nombre}</h1>`);
+  ventanaNotas.document.write(`<p>Parcial 1: ${alumno.parcial1}</p>`);
+  ventanaNotas.document.write(`<p>Parcial 2: ${alumno.parcial2}</p>`);
+  ventanaNotas.document.write(`<p>Promedio: ${calcularPromedio(alumno)}</p>`);
 }
 
-const alumnos = [];
-for (let i = 0; i < alumnosInscriptos.length; i++) {
-  const nombreAlumno = alumnosInscriptos[i];
-  const alumno = new Alumno(nombreAlumno);
-  alumnos.push(alumno);
+function calcularPromedio(alumno) {
+  const promedio = (alumno.parcial1 + alumno.parcial2) / 2;
+  return promedio.toFixed(2);
 }
 
-document.write("<br>"+"Promedios de los alumnos:<br>");
-for (const alumno of alumnos) {
-  const promedio = alumno.calcularPromedio();
-  document.write(`${alumno.nombre}: ${promedio}<br>`);
-}
+// Guardar en el almacenamiento local
+localStorage.setItem('alumnosInscriptos', JSON.stringify(alumnosInscriptos));
